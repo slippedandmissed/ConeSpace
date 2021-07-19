5309ed2eeb1ff1deee2459efc1701b9dd6f29e26
slippedandmissed <gbt505@hotmail.com>
2021-07-19 06:08:29 +0100

    Used ugly hacky workaround to make the cookie-dragging flash less on mobile.
    Basically instead of keeping generating an array of ghosts and ngForing through it,
    I now use the ghosts service to programatically create, update, and destroy DOM
    objects. This means that the cookie images aren't being deleted and recreated several times per second.

--- frontend/src/app/services/ghosts.service.ts
+++ frontend/src/app/services/ghosts.service.ts
@@ -18,8 +18,13 @@ export class GhostsService {
 
   private ghosts: { [key: string]: Ghost } = {};
 
+  private generateSelector(id: string): string {
+    return `#ghost_${btoa(id).replace(/=/g, "_")}`;
+  }
+
   public createGhost(id: string, img: string, type: ghostType, x: number, y: number, reemit: boolean = true) {
     this.ghosts[id] = { img, x, y, type, active: true };
+    this.drawGhosts();
     if (reemit)
       this.api.socketEmit("createGhost", id, img, type, x / document.body.clientWidth, y / document.body.clientHeight);
   }
@@ -27,12 +32,14 @@ export class GhostsService {
 
   public updateGhost(id: string, x: number, y: number, reemit: boolean = true) {
     this.ghosts[id] = { ...this.ghosts[id], x, y };
+    this.drawGhosts();
     if (reemit)
       this.api.socketEmit("updateGhost", id, x / document.body.clientWidth, y / document.body.clientHeight);
   }
 
   public deleteGhost(id: string, reemit: boolean = true) {
     this.ghosts[id].active = false;
+    this.drawGhosts();
     const audio: HTMLAudioElement | null = document.querySelector("audio");
     if (audio) {
       if (this.ghosts[id].type === "food") {
@@ -46,12 +53,38 @@ export class GhostsService {
     setTimeout(() => {
       if (this.ghosts[id] && !this.ghosts[id].active) {
         delete this.ghosts[id];
+        document.querySelector(this.generateSelector(id))?.remove();
       }
     }, 1000);
   }
 
-  public getGhosts(): Ghost[] {
+  public getGhosts(): Ghost [] {
     return Object.values(this.ghosts);
+  }
+
+  private drawGhosts() {
+    const container = document.querySelector("#interactions");
+    if (!container) {
+      return;
+    }
+    for (const id in this.ghosts) {
+      const ghost = this.ghosts[id];
+      let obj: HTMLImageElement | null = document.querySelector(this.generateSelector(id));
+      if (!obj) {
+        obj = document.createElement("img");
+        container.appendChild(obj);
+        obj.classList.add("ghost");
+        obj.src = ghost.img;
+        obj.id = this.generateSelector(id).substring(1);
+      }
+      obj.style.top = `${ghost.y}px`;
+      obj.style.left = `${ghost.x}px`;
+      if (ghost.active) {
+        obj.classList.remove("hidden");
+      } else {
+        obj.classList.add("hidden");
+      }
+    }
   }
 
   constructor(private api: ApiService) {
